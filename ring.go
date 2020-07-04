3d6068097d3e4af5692c303e258cd0dac2bbe383
Manoj Ghosh <manoj.ghosh@oracle.com>
2020-07-03 21:52:54 -0700

    update the readme

--- ring.go
+++ ring.go
@@ -1,8 +1,8 @@
 package ring
 
 // RingChannel uses two channels input and output in a way that never blocks the input.
-// Specifically, if a value is written to a RingChannel's input when its buffer is full then the oldest
-// value in the output channel is discarded to make room (just like a standard ring-buffer).
+// If a new value gets added to a RingChannel's input when its buffer is full then the oldest
+// value in the output channel gets removed to make room (just like a standard ring-buffer).
 
 type RingChannel struct {
 	input  chan interface{}
